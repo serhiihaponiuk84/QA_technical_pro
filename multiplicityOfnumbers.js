643ea23a932033a50e241df9b932f4fbf5c15bc0
serhiihaponiuk <sergey.gaponyuk@gmail.com>
2024-03-29 23:23:53 +0200

    improving the code based on the teacher's recommendations

--- multiplicityOfnumbers.js
+++ multiplicityOfnumbers.js
@@ -2,15 +2,23 @@ const divisor = 3;
 let min_number = 1;
 let max_number = 100;
 
-function multiplicityOfnumbers(min_number= 1, max_number = 100, divisor = 2) {
+function addNonMultipleNumbers(min_number= 1, max_number = 100, divisor = 2) {
     let temp_number;
     let sum = 0;
-    if (isNaN(min_number) || isNaN(max_number) || isNaN(divisor)) return NaN;
-    if (divisor == 0) return NaN;
+    if (isNaN(min_number) || isNaN(max_number) || isNaN(divisor)) {
+        console.log("Программа обробляє лише числа.");
+        return NaN;
+    };
+    if (divisor == 0) {
+        console.log("Не можна ділити на 0!");
+        return NaN;
+    };
     if (min_number > max_number) {
+        console.log("Діапазон не може починатися з " + min_number + " та закінчуватися " + max_number + ".");
         temp_number = min_number;
         min_number = max_number;
         max_number = temp_number;
+        console.log("Граничні значення діапазоні змінені місцями.");
     }
     for (let i= min_number; i <= max_number; i++) {
         if (i % divisor) sum += i;
@@ -18,9 +26,6 @@ function multiplicityOfnumbers(min_number= 1, max_number = 100, divisor = 2) {
     return sum;
 }
 
-let result = multiplicityOfnumbers(max_number, min_number, divisor);
-if (isNaN(result)) {
-    console.log("Помилка вхідних даних!")
-} else {
+let result = addNonMultipleNumbers(max_number, min_number, divisor);
+if !(isNaN(result))
     console.log("Cума чисел від " + min_number +" до " + max_number + " включно, які не кратні " + divisor + " дорівнює " + result);
-}
