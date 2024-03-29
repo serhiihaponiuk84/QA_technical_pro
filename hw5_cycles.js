85e5cd7c8920c53e63da80dc7d298b61f6dc2cb0
serhiihaponiuk <sergey.gaponyuk@gmail.com>
2024-03-30 01:11:16 +0200

    implementation lecture5.js and hw5_cycles.js, hw5_functions.js

---
+++ hw5_cycles.js
@@ -0,0 +1,52 @@
+/* Task - 1
+   Replace the "for" loop with a "while" loop
+*/
+
+for (let i = 0; i < 8; i++) {
+    console.log( `число - ` + i);
+}
+
+/*Task - 2
+  Implement algorithm which will calculate sum of numbers from firstNumber to secondNumber (included)
+
+  Use for loop
+*/
+
+let firstNumber = -100;
+let secondNumber = 250;
+
+/* Task - 3
+  Make a timer that will count from 10 to 0
+
+
+! ! ! https://www.tiktok.com/@devslopes/video/7146708519427820843 ! ! !
+
+  For example,
+  10 seconds left
+  9 seconds left
+  8 seconds left
+*/
+
+/* Task - 4
+  Create program to display the following sequence: 7 14 21 28 35 42 49 */
+
+/* Task - 5
+  implement algorithm which calculates factorial for
+  using for, while, do..while operators
+
+
+  what is factorial: https://www.mathsisfun.com/numbers/factorial.html
+*/
+
+/* Task - 6
+  implement algorithm filling a bathtub using buckets.
+  we know how many liters the bathtub holds and the capacity of one bucket,
+  but we don't know how many buckets we need.
+  We need to fill the bathtub without overflowing it.
+
+  Use while loop
+*/
+
+let bucket = 5; //capacity of one bucket
+let initialBathState = 0; //the initial value of the amount of water in the bathroom
+let bathCapacity = 60; //
---
+++ hw5_functions.js
@@ -0,0 +1,35 @@
+/* Task 1
+    Write a function min(a, b) that returns the lesser of two numbers a and b.
+
+    For example,
+    min(2, 5) == 2
+    min(3, -1) == -1
+    min(1, 1) == 1
+*/
+
+/* Task 2
+   Create a variable which will return a function
+    which will take parameter as a number (where 1 stands for pizza, 2 is spagetti and 0 is ramen)
+    and returns text ee.g. "My lunch for today is pizza"
+
+    Use function expression with arrow function notation
+
+    @ @ @ https://www.tiktok.com/@devslopes/video/7332200215258238251 @ @ @
+*/
+
+/* Task 3
+    Callback
+    Create a function (e.g. function1)  which will take another function (e.g. function2) as an argument
+    and returns result of execution of that another function in string format
+
+    Example,
+    if function2 returns "THIS IS THE RESULT OF CALLBACK FUNCTION"
+    than function1 should return "The result = THIS IS THE RESULT OF CALLBACK FUNCTION"
+*/
+
+/* Task 4
+    Create a function task13Func which returns word google with given numbers of "o" symbols
+
+    Example,
+    funcName(7) // => gooooooogle
+*/
