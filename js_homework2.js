e426ebb6c2b3fb987ec1950cf2b9059e200f7cce
serhiihaponiuk <sergey.gaponyuk@gmail.com>
2024-03-29 23:00:10 +0200

    added a check for an empty term, bug fix: year cannot be negative or zero

--- js_homework2.js
+++ js_homework2.js
@@ -26,21 +26,26 @@ const main = async () => {
 
     let YearWord;
 
-    if (isNaN(age) || (!age)) YearWord = 'не є роком.'
+    if (isNaN(age) || (!age) ) YearWord = 'не є роком.'
         else {
-
-            let n = Math.abs(age) % 100;
-            let n1 = n % 10;
-
-            if (n > 10 && n < 20) YearWord = 'років';
+            if (age <= 0) {
+                YearWord = "Рік не може бути від'ємним або дорівнювати нулю.";
+                age = "";
+            }
             else {
-                if (n1 > 1 && n1 < 5) YearWord = 'роки';
+                let n = Math.abs(age) % 100;
+                let n1 = n % 10;
+
+                if (n > 10 && n < 20) YearWord = ' років';
                 else {
-                    if (n1 == 1) YearWord = 'рік'; else YearWord = 'років';
+                    if (n1 > 1 && n1 < 5) YearWord = ' роки';
+                    else {
+                        if (n1 == 1) YearWord = ' рік'; else YearWord = ' років';
+                    }
                 }
             }
         }
-        console.log(age + " " + YearWord);
+        console.log(age + YearWord);
 
     rl.close();
 };
