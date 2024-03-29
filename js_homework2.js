395b8f185de8d098f93c04a92540233e2cace17e
serhiihaponiuk <sergey.gaponyuk@gmail.com>
2024-03-29 18:04:42 +0200

    implementation hw2

--- js_homework2.js
+++ js_homework2.js
@@ -11,3 +11,36 @@
 1. Створіть гілку js_homework2.
 2. Додати туди ваше домашне завдання
 3. Вислати вашу гілку */
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const question = (prompt) => new Promise((resolve) => rl.question(prompt, resolve));
+
+const main = async () => {
+    const age = await question("Hi, how old are you?");
+
+    let YearWord;
+    let n = Math.abs(age) % 100;
+    let n1 = n % 10;
+
+    if (isNaN(age)) YearWord = 'не є роком.'
+        else {
+            if (n > 10 && n < 20) YearWord = 'років';
+            else {
+                if (n1 > 1 && n1 < 5) YearWord = 'роки';
+                else {
+                    if (n1 == 1) YearWord = 'рік'; else YearWord = 'років';
+                }
+            }
+        }
+        console.log(age + " " + YearWord);
+
+    rl.close();
+};
+
+main(); // Виклик асинхронної функції
