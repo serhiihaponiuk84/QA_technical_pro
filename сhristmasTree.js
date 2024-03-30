4ef19ded4b9412cafb87d0eb18b5b98e8a5f6ca4
serhiihaponiuk <sergey.gaponyuk@gmail.com>
2024-03-30 14:23:47 +0200

    improving the code based on the teacher's recommendations

--- сhristmasTree.js
+++ сhristmasTree.js
@@ -2,18 +2,18 @@ let heightOftriangle = 5;
 let drawingSymbol = "*";
 
 function drawChristmasTree (heightOftriangle = 5, drawingSymbol = "*") {
-    if (isNaN(heightOftriangle) || (!heightOftriangle)) {
-        console.log("Висота трикутника повинна бути числом.");
+    if (isNaN(heightOftriangle) || (heightOftriangle < 2)) {
+        console.log("Висота трикутника повинна бути числом більше 1, інакше кінцева фігура не буде схожа на трикутник.");
         console.log("Для малювання трикутника буде використано висота за умовчанням.");
         drawChristmasTree(undefined, drawingSymbol);
         return;
     }
-    if (heightOftriangle < 2) {
-        console.log("Висота трикутника повинна бути більше 1, інакше кінцева фігура не буде схожа на трикутник.");
-        console.log("Для малювання трикутника буде використано висота за умовчанням.");
-        drawChristmasTree(undefined, drawingSymbol);
-        return;
-    }
+    // if (heightOftriangle < 2) {
+    //     console.log("Висота трикутника повинна бути більше 1, інакше кінцева фігура не буде схожа на трикутник.");
+    //     console.log("Для малювання трикутника буде використано висота за умовчанням.");
+    //     drawChristmasTree(undefined, drawingSymbol);
+    //     return;
+    // }
     if (!drawingSymbol) {
         console.log("Не можна намалювати трукутник, використовуючи замість символа для малювання порожній рядок.");
         console.log("Для малювання трикутника буде використано символ за умовчанням.");
@@ -38,4 +38,6 @@ drawChristmasTree(heightOftriangle, drawingSymbol)
 drawChristmasTree();
 drawChristmasTree(-1, "+++");
 drawChristmasTree(3, "q");
+drawChristmasTree(3, "");
+drawChristmasTree(0, "");
 drawChristmasTree(undefined, 505);
