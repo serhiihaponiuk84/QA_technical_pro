00e7ce4e34a4c9be1bc910df0d941d37edb1b68c
serhiihaponiuk <sergey.gaponyuk@gmail.com>
2024-04-03 11:46:58 +0300

    implementation hw isNaNfunction(value)

--- isNaN.js
+++ isNaN.js
@@ -6,10 +6,10 @@ function isNaNfunction(value) {
     return +value !== +value;
 }
 
-console.log(isNaNfunction(NaN));            // true
-console.log(isNaNfunction("text"));   // true, так как "text" не может быть преобразовано в число
-console.log(isNaNfunction(123));      // false, 123 является числом
-console.log(isNaNfunction("123"));    // false, строка "123" может быть преобразована в число
-console.log(isNaNfunction(undefined));// true, undefined преобразуется в NaN
-console.log(isNaNfunction({}));       // true, объект не может быть преобразован в число
-console.log(isNaNfunction(true));     // false, логическое true преобразуется в 1, что является числом
+console.log(isNaNfunction(NaN));
+console.log(isNaNfunction("test"));
+console.log(isNaNfunction(123));
+console.log(isNaNfunction("456"));
+console.log(isNaNfunction(undefined));
+console.log(isNaNfunction({}));
+console.log(isNaNfunction(true));
