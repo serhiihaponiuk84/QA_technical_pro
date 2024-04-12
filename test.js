5988598e5f3487a6b4b12a16f8bb9a0e0f4fddf8
serhiihaponiuk <sergey.gaponyuk@gmail.com>
2024-04-12 19:38:06 +0300

    implementation test#15

--- test.js
+++ test.js
@@ -1,3 +1,31 @@
-str = "100 грн";
-let value = Number(str.replace("грн","").trim());
-console.log(value);
+const footballTeam = {
+    teamName: 'Динамо Київ',
+    country: 'Україна',
+    yearFounded: 1927,
+    players: [
+        {name: 'Ігор', age: 28},
+        {name: 'Василь', age: 25},
+        {name: 'Олександр', age: 30},
+    ],
+    displayTeamInfo() {
+        console.log(`Назва команди: ${this.teamName}`);
+        console.log(`Країна: ${this.country}`);
+        console.log(`Рік заснування: ${this.yearFounded}`);
+        console.log('Список гравців:');
+        this.players.forEach(player => {
+            console.log(`Ім'я: ${player.name}, Вік: ${player.age}`);
+        });
+    },
+    getInfo() {
+        Info = "";
+        for (let characteristic in this) {
+            if (typeof this[characteristic] !== 'function') {
+                Info += characteristic + ": " + this[characteristic] + "\n";
+            }
+        }
+        return Info;
+    }
+}
+footballTeam.displayTeamInfo();
+console.log();
+console.log(footballTeam.getInfo());
