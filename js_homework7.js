9414a2f9781e2a58f2f3204d81975c653ed9d7cc
serhiihaponiuk <sergey.gaponyuk@gmail.com>
2024-04-19 22:12:15 +0300

    implementation RegExp (hw)

--- js_homework7.js
+++ js_homework7.js
@@ -19,24 +19,36 @@ let pattern = /^[^Aa]{6,}$/
 Опис ДЗ: В нас є масив обєктів в яких міститься email.
 У нас параноя, ми не довіряємо жодним адресам, окрім зареєстрованих на доменах gmail.com та yahoo.com.
 За допомогою регулярного виразу створіть масив з адресами, гідними довіри. Постарайтеся також зробити
-просту валідацію до @
-одне, або два слова які можуть містити (латинські букви, цифри) яке може бути розділене крапкою.
+просту валідацію до @ одне, або два слова які можуть містити (латинські букви, цифри) яке може бути розділене крапкою.
  */
 
-var arr = [
+let arr = [
     {
-        userName:"Test",
-        lastName:"Test",
-        email:"test.test@gmail.com>"
-        },
+        userName: "Test",
+        lastName: "Test",
+        email: "test.test@gmail.com"
+    },
     {
-        userName:"Dmitro",
-        lastName:"Porohov",
-        email:"dmitro.porohov@yahoo.com>"
-            },
+        userName: "Dmitro",
+        lastName: "Porohov",
+        email: "dmitro.porohov@yahoo.com"
+    },
     {
-        userName:"Andrii",
-        lastName:"",
-        email:"andrii@mail.ru>" // Нам такі не підходять
-            },
-    ];
+        userName: "Andrii",
+        lastName: "",
+        email: "andrii@mail.ru" // Нам такі не підходять
+    },
+];
+
+// let emailRegex = /^[a-zA-Z0-9._%+-]+?@(gmail|yahoo)\.com$/;
+let emailRegex = /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@(gmail|yahoo)\.com$/;
+
+let trustedEmails = [];
+
+arr.forEach(item => {
+    if (emailRegex.test(item.email)) {
+        trustedEmails.push(item.email);
+    }
+});
+
+console.log(trustedEmails);
